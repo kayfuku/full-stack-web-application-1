0cb795fee8b2579da4f4f9d94ec94db71471322a
Kei Fukutani <kifinewera@gmail.com>
2020-12-16 17:42:05 -0800

    database settings

--- config.py
+++ config.py
@@ -10,4 +10,10 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+DATABASE_NAME = 'fyyur'
+username = 'kei'
+password = ''
+url = 'localhost:5432'
+SQLALCHEMY_DATABASE_URI = "postgres://{}:{}@{}/{}".format(username, password, url, DATABASE_NAME)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
